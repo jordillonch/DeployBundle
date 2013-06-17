3c6cf9d3dad976b582b572b9c09de2afc3c3d01c
Jordi Llonch <llonch.jordi@gmail.com>
2013-06-17 16:14:04 +0200

    remote exec to localhost bypassed to execute command

--- Library/BaseDeployer.php
+++ Library/BaseDeployer.php
@@ -423,7 +423,8 @@ abstract class BaseDeployer
         foreach ($servers as $server)
         {
             list($host, $port) = $this->extractHostPort($server);
-            $sshCommand = 'ssh -t -p ' . $port . ' -o "LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ' . $host . ' "' . $command . '"';
+            if($host == 'localhost') $sshCommand = $command;
+            else $sshCommand = 'ssh -t -p ' . $port . ' -o "LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ' . $host . ' "' . $command . '"';
             if($this->dryMode) $this->output->writeln($sshCommand);
             else $this->exec($sshCommand);
         }
